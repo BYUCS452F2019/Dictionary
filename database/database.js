8d930b063fe76b566a265e41e2ab9aca46687c68
Eric Burdett <eric@burdett.us>
2019-10-02 17:36:06 -0600

    Added postgres database to app with basic hello world query endpoint

---
+++ database/database.js
@@ -0,0 +1,29 @@
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL || "postgres://postgres:admin@localhost:5432/postgres";
+const url = require('url');
+
+module.exports = {
+   helloWorldQuery: (req, res) => {
+      var client = new pg.Client(connectionString);
+      client.connect(pgConnectCallback);
+
+      client.query('SELECT ''Hello World from PostgreSQL''', (error, result) => {
+         if (error) {
+            console.log("Error in query: ");
+            console.log(error);
+	 }
+         else {
+	    res.json(result.rows);
+	 }
+      }
+   }
+}
+
+function pgConnectCallback(error) {
+   if (error) {
+      console.log("Error connection to postgres: ");
+      console.log(error);
+   }
+   else {
+      console.log("Successfully connected to postgres");
+   }
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require('./database/database');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -6,6 +7,7 @@ app.set('port', PORT)
    .get('/', (req, res) => {
        res.send('Hello World');
    })
+   .get('/pg', db.helloWorldQuery)
    .listen(app.get('port'), () => {
        console.log('Listening on port: ' + app.get('port'));
    });
