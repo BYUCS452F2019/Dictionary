f343d5562779214e11123b6207b07d63918a661c
Eric Burdett <eric@burdett.us>
2019-10-31 10:38:21 -0600

    Fixed merge conflict on server.js file

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.set('port', PORT)
-<<<<<<< HEAD
     .use(express.static('static'))
-    .get('/pg', db.helloWorldQuery)
     .get('/searchWords', db.searchWords)
     .get('/addWord', db.addWord)
     .get('/deleteWord', db.deleteWord)
@@ -17,15 +15,6 @@ app.set('port', PORT)
     .get('/deleteRelatedWord', db.deleteRelatedWord)
     .get('/getRelatedWords', db.getRelatedWords)
 
-
     .listen(app.get('port'), () => {
         console.log('Listening on port: ' + app.get('port'));
     });
-=======
-   .use(express.static('static'))
-   .get('/searchWords', db.searchWords)
-   .get('/getRelatedWords', db.getRelatedWords)
-   .listen(app.get('port'), () => {
-       console.log('Listening on port: ' + app.get('port'));
-   });
->>>>>>> Fixed errors
