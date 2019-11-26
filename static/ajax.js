ac3e11a92328d281eaa475293b2527960db021d4
Eric Burdett <eric@burdett.us>
2019-11-25 17:59:30 -0700

    Fixed front-end search interface

--- static/ajax.js
+++ static/ajax.js
@@ -17,32 +17,80 @@ function showWords() {
 
                 console.log(result);
 
+                var currentWord;
+                var currentLanguage;
+                var relatedText = '';
+                var wordsExist = false;
+
                 for (var i = 0; i < Object.keys(result).length; ++i) {
-                    if (i % 4 == 0) {
-                        html += '<div class="row">'
+
+                    // if (result[i]._fields[0].properties.name == result[i]._fields[1].properties.name) {
+                    //     continue;
+                    // }
+
+                    if (currentWord != result[i]._fields[0].properties.name) {
+                        console.log('In main if:');
+                        console.log('currentWord=' + currentWord);
+                        console.log('nextWord=' + result[i]._fields[0].properties.name);
+                        if (wordsExist) {
+                            var related;
+                            if (relatedText) {
+                                related = '<br>Related words: <ul>' + relatedText + '<ul>'
+                            }
+                            else {
+                                related = '';
+                            }
+
+                            console.log("related=" + related);
+
+                            html += `<div class="card">
+                                        <div class="card-body">
+                                            <h5 class="card-title">` + currentWord + `</h5>
+                                            <p class="card-text">Language: ` + currentLanguage +
+                                related + `</p>
+                                        </div>
+                                     </div>`;
+
+                            relatedText = '';
+                        }
+
+                        wordsExist = true;
+                        currentWord = result[i]._fields[0].properties.name;
+                        currentLanguage = result[i]._fields[0].properties.language;
                     }
 
-                    var relatedWords = result[i].related_words;
-                    var relatedText = '<ul>';
-                    for (var j = 0; j < Object.keys(relatedWords).length; ++j) {
-                        relatedText += '<li>' + relatedWords[j].word + ' (' + relatedWords[j].lang + ')' + '</li>';
-                    }
-                    relatedText += '</ul>';
+                    var relatedWord = result[i]._fields[1].properties.name;
+                    var relatedLanguage = result[i]._fields[1].properties.language;
 
-                    html += `<div class="col-lg-3 col-md-2 col-sm-12"><div class="card">
-                               <div class="card-body">
-                                 <h5 class="card-title">` + result[i].word + `</h5>
-                                 <p class="card-text">Language: ` + result[i].lang + `<br><br>
-                                  Related Words: ` + relatedText + `</p>
-                               </div>
-                             </div></div>`;
-
-                    if (i % 5 == 0 || i == Object.keys(result).length) {
-                        html += '</div>'
+                    if (currentWord && currentWord != relatedWord) {
+                        relatedText += '<li>' + relatedWord + ' (' + relatedLanguage + ')' + '</li>';
+                        console.log("relatedText=" + relatedText);
                     }
+
                 }
 
-                html += `</div>`;//</div>`;
+                // Add the last cards element if there has been at least one word added
+                if (wordsExist) {
+                    var related;
+                    if (relatedText) {
+                        related = '<br>Related words: <ul>' + relatedText + '<ul>'
+                    }
+                    else {
+                        related = '';
+                    }
+
+                    console.log("related=" + related);
+
+                    html += `<div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">` + currentWord + `</h5>
+                                    <p class="card-text">Language: ` + currentLanguage +
+                                    related + `</p>
+                                </div>
+                             </div>`;
+                }
+
+                html += `</div>`;
 
                 document.getElementById("searchResults").innerHTML = html;
                 console.log("Success")
